725432dbf57dfebdfd014fe9a6b2936380a382ba
sanricl <sandersonricl@gmail.com>
2022-09-23 05:49:27 -0300

    Served Front-end in Production. Sibe by Side w/ back-end

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -10,6 +11,10 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
