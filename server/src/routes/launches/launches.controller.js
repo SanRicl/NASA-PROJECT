3c327554fd07cadbcac42001118961afb5db30bd
sanricl <sandersonricl@gmail.com>
2022-09-24 05:43:54 -0300

    Validation for POST /launches request

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -8,9 +8,27 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
 
-  launch.launchDate = new Date(launch.launchDate);
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    //400 status - Bad Request - something is invalid
+    return res.status(400).json({
+      error: "Missing required launch property.",
+    });
+  }
 
+  launch.launchDate = new Date(launch.launchDate);
+  //When we set a date, Javascript transform it into a number format. So, if the data passed through the input wasn't a number, it will be a NaN, and if this is true, it won't permit you to save the data and it will throw an error.
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
   addNewLaunch(launch);
+
   //201 - Created
   return res.status(201).json(launch);
 }
