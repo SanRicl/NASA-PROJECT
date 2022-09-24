325a400814910f9a1dd58d66404dfdfadd401519
sanricl <sandersonricl@gmail.com>
2022-09-24 05:21:25 -0300

    Created Launches Post Method. Cleanned end-points for planets and launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,10 +1,12 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
   rocket: "Explores IS1",
-  louchDate: new Date("December 27, 2030"),
+  lauchDate: new Date("December 27, 2030"),
   destination: "Kepler-442 b",
   costumers: ["NASA", "ZTM"],
   upcoming: true,
@@ -17,6 +19,22 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+//addNewLauch will receive the data from the client, then will take the latestFlightNumber, that was setted by default "100" and increase +1. This will be the key or the number of the flight that will indetify the actual flight that is beeing added. Than it will be setted with the latestFlightNumber lauches.set() with the informations bellow inside the function. Success, Upcoming and customers values, will always be setted by default. Only it will be added the informations from the input that corresponds to the fields: mission, rocket, launchDate and destination.
+
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ['Zero to Mastery', 'NASA'],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 };
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,8 +1,18 @@
-const { getAllLaunches } = require("../../models/launches.model");
+const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
   //returning an array from launches
-  return res.status(200).json(getAllLaunches);
+  return res.status(200).json(getAllLaunches());
 }
 
-module.exports = { httpGetAllLaunches };
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  launch.launchDate = new Date(launch.launchDate);
+
+  addNewLaunch(launch);
+  //201 - Created
+  return res.status(201).json(launch);
+}
+
+module.exports = { httpGetAllLaunches, httpAddNewLaunch };
