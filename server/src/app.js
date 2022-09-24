1c2b094eeb828fbef050c9c160576a258c8b9902
sanricl <sandersonricl@gmail.com>
2022-09-23 21:22:08 -0300

    Launches schema. Routes, Controllers and Model setted up

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 app.use(
   cors({
@@ -16,7 +17,10 @@ app.use(morgan("combined"));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.use(launchesRouter);
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
---
+++ server/src/models/launches.model.js
@@ -0,0 +1,18 @@
+const launches = new Map();
+
+const launch = {
+  flightNumber: 100,
+  mission: "Kepler Exploration X",
+  rocket: "Explores IS1",
+  louchDate: new Date("December 27, 2030"),
+  destination: "Kepler-442 b",
+  costumers: ["NASA", "ZTM"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+module.exports = {
+  launches,
+};
