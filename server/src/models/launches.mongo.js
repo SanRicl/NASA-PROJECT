4fcb12a81b912e435559a3dfd5b0e2512e19f26c
sanricl <sandersonricl@gmail.com>
2022-09-26 07:50:42 -0300

    chore: create models from schemas

--- server/src/models/launches.mongo.js
+++ server/src/models/launches.mongo.js
@@ -32,3 +32,6 @@ const launchesSchema = new mongoose.Schema({
     default: true,
   },
 });
+
+//always in singular. It will connects launchesSchema with the 'launches' collection
+module.exports = mongoose.model("Launch", launchesSchema);
