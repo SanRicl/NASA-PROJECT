2e437531fa4e0b340168a4b2a6995b779c1a2df6
sanricl <sandersonricl@gmail.com>
2022-10-03 17:50:48 -0300

    chore: sort paginated data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -4,19 +4,6 @@ const planets = require("./planets.mongo");
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-  flightNumber: 100, //flight_number
-  mission: "Kepler Exploration X", //name
-  rocket: "Explores IS1", //rocket.name
-  lauchDate: new Date("December 27, 2030"), //date_local
-  target: "Kepler-442 b", //not applicable
-  customers: ["NASA", "ZTM"], //payload .customers for each payload
-  upcoming: true, //upcoming
-  success: true, //success
-};
-
-saveLaunch(launch);
-
 const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
 
 async function populateLaunches() {
@@ -116,6 +103,9 @@ async function getAllLaunches(skip, limit) {
         _id: 0,
       }
     )
+    .sort({
+      flightNumber: 1,
+    })
     .skip(skip)
     .limit(limit);
 }
